4320438056f85c8dce88bde7803b54c720408797
Glitch (decisive-dove) <none>
2019-11-18 18:36:46 +0000

    🎈🌉 Checkpoint ./server.js:7351991/1878 ./routes/api.js:7351991/4

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ var runner            = require('./test-runner');
 
 var app = express();
 
+app.use(helmet({
+  contentSecurityPolicy: {directives: {
+    defaultSrc: ["'self'"],
+    imgSrc: ["'self'", 'https://hyperdev.com/', 'https://cdn.gomix.com/', 'http://glitch.com/'],
+    scriptSrc: ["'self'", "'unsafe-inline'", 'https://code.jquery.com/'],
+    styleSrc: ["'self'", "'unsafe-inline'"]
+  }}
+}))
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
@@ -20,37 +29,45 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
+MongoClient.connect(process.env.DB, {useUnifiedTopology: true}, function(err, client){
+  
+  //Sample front-end
+  app.route('/b/:board/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/board.html');
+    });
+  app.route('/b/:board/:threadid')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/thread.html');
+    });
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  //Index page (static HTML)
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
+  
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
+  
+  //Routing for API 
+  apiRoutes(app, client.db('board'));
+  
+})
+
+
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);
 
-//Sample Front-end
 
     
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
+
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
