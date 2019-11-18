dd3462ddd58e45bfe10b278dca84cc5572b96b0a
Glitch (decisive-dove) <none>
2019-11-18 23:37:38 +0000

    ⛸👯 Checkpoint ./routes/api.js:7351991/340

--- routes/api.js
+++ routes/api.js
@@ -15,7 +15,16 @@ module.exports = function (app, db) {
   
   app.route('/api/threads/:board')
   .post(function(req, res){
-    db.updateOne
+    db.collection('boards')
+    .updateOne(
+      {board: req.params.board},
+      {
+      $setOnInsert: {
+        board: req.params.board,
+        message: req.body.text,
+        password: req.body.password
+      }
+    })
   })
     
   app.route('/api/replies/:board');
